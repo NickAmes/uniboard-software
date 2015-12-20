2dcc5c086c1c51c771a06ea1acc5af893beafaa9
Nick Ames <nick@fetchmodus.org>
2015-12-20 07:25:45 -0800

    Power supply test and loopback test program. Togggles stepper control lines at high frequencies while coping characters from serial RX to TX.

--- hdl/uniboard.sv
+++ hdl/uniboard.sv
@@ -6,7 +6,7 @@ module UniboardTop(
 	input logic uart_rx, /* UART input from control computer. */
 	output logic uart_tx, /* UART output to control computer. */
 	output logic status_led[2:0],
-	input logic clk_20MHz,
+	input logic clk_12MHz,
 	
     output logic Stepper_X_Step,
     output logic Stepper_X_Dir,
@@ -14,28 +14,31 @@ module UniboardTop(
     output logic Stepper_X_M1,
     output logic Stepper_X_M2,
     output logic Stepper_X_En,
-    output logic Stepper_X_nFault,
+    input logic Stepper_X_nFault,
+    
     output logic Stepper_Y_Step,
     output logic Stepper_Y_Dir,
     output logic Stepper_Y_M0,
     output logic Stepper_Y_M1,
     output logic Stepper_Y_M2,
     output logic Stepper_Y_En,
-    output logic Stepper_Y_nFault,
+    input logic Stepper_Y_nFault,
+    
     output logic Stepper_Z_Step,
     output logic Stepper_Z_Dir,
     output logic Stepper_Z_M0,
     output logic Stepper_Z_M1,
     output logic Stepper_Z_M2,
     output logic Stepper_Z_En,
-    output logic Stepper_Z_nFault,
+    input logic Stepper_Z_nFault,
+    
     output logic Stepper_A_Step,
     output logic Stepper_A_Dir,
     output logic Stepper_A_M0,
     output logic Stepper_A_M1,
     output logic Stepper_A_M2,
     output logic Stepper_A_En,
-    output logic Stepper_A_nFault,
+    input logic Stepper_A_nFault,
 	
 	input logic limit[3:0],
 	output logic expansion1,
@@ -52,49 +55,78 @@ module UniboardTop(
 	input logic encoder_lb,
 	input logic encoder_li,
 	
-	input logic rc1,
-	input logic rc2,
-	input logic rc3,
-	input logic rc4,
-	input logic rc7,
-	input logic rc8,
+	input logic rc_ch1,
+	input logic rc_ch2,
+	input logic rc_ch3,
+	input logic rc_ch4,
+	input logic rc_ch7,
+	input logic rc_ch8,
 	
 	output logic motor_pwm_l,
 	output logic motor_pwm_r,
-	input logic pause
+	input logic pause,
+	
+	inout debug[8:0]
 	
 	);
 	
-	assign Stepper_X_Step = 0;
-	assign Stepper_X_Dir = 0;
-	assign Stepper_X_M0 = 0;
-	assign Stepper_X_M1 = 0;
-	assign Stepper_X_M2 = 0;
-	assign Stepper_X_En = 0;
-	assign Stepper_X_nFault = 0;
-	assign Stepper_Y_Step = 0;
-	assign Stepper_Y_Dir = 0;
-	assign Stepper_Y_M0 = 0;
-	assign Stepper_Y_M1 = 0;
-	assign Stepper_Y_M2 = 0;
-	assign Stepper_Y_En = 0;
-	assign Stepper_Y_nFault = 0;
-	assign Stepper_Z_Step = 0;
-	assign Stepper_Z_Dir = 0;
-	assign Stepper_Z_M0 = 0;
-	assign Stepper_Z_M1 = 0;
-	assign Stepper_Z_M2 = 0;
-	assign Stepper_Z_En = 0;
-	assign Stepper_Z_nFault = 0;
-	assign Stepper_A_Step = 0;
-	assign Stepper_A_Dir = 0;
-	assign Stepper_A_M0 = 0;
-	assign Stepper_A_M1 = 0;
-	assign Stepper_A_M2 = 0;
-	assign Stepper_A_En = 0;
-	assign Stepper_A_nFault = 0;
+// 	assign Stepper_X_Step = 0;
+// 	assign Stepper_X_Dir = 0;
+// 	assign Stepper_X_M0 = 0;
+// 	assign Stepper_X_M1 = 0;
+// 	assign Stepper_X_M2 = 0;
+// 	assign Stepper_X_En = 0;
+// 	assign Stepper_Y_Step = 0;
+// 	assign Stepper_Y_Dir = 0;
+// 	assign Stepper_Y_M0 = 0;
+// 	assign Stepper_Y_M1 = 0;
+// 	assign Stepper_Y_M2 = 0;
+// 	assign Stepper_Y_En = 0;
+// 	assign Stepper_Z_Step = 0;
+// 	assign Stepper_Z_Dir = 0;
+// 	assign Stepper_Z_M0 = 0;
+// 	assign Stepper_Z_M1 = 0;
+// 	assign Stepper_Z_M2 = 0;
+// 	assign Stepper_Z_En = 0;
+// 	assign Stepper_A_Step = 0;
+// 	assign Stepper_A_Dir = 0;
+// 	assign Stepper_A_M0 = 0;
+// 	assign Stepper_A_M1 = 0;
+// 	assign Stepper_A_M2 = 0;
+// 	assign Stepper_A_En = 0;
 	
-	assign status_led[0] = (& limit)& pause  & encoder_ra& encoder_rb& encoder_ri& encoder_la& encoder_lb & encoder_li&rc1&rc2&rc3&rc4&rc7&rc8;
+	logic noise_12Mhz, noise_1Mhz, noise_100khz, noise_10khz;
+	assign noise_12Mhz = clk_12MHz;
+	ClockDividerP #(12) DivA(clk_12MHz, noise_1Mhz, 0);
+	ClockDividerP #(120) DivB(clk_12MHz, noise_100khz, 0);
+	ClockDividerP #(1200) DivC(clk_12MHz, noise_10khz, 0);
+	
+	assign Stepper_X_Step = noise_12Mhz;
+	assign Stepper_X_Dir = ~noise_12Mhz;
+	assign Stepper_X_M0 = noise_12Mhz;
+	assign Stepper_X_M1 = ~noise_12Mhz;
+	assign Stepper_X_M2 = noise_12Mhz;
+	assign Stepper_X_En = ~noise_12Mhz;
+	assign Stepper_Y_Step = noise_10khz;
+	assign Stepper_Y_Dir = noise_100khz;
+	assign Stepper_Y_M0 = ~noise_10khz;
+	assign Stepper_Y_M1 = noise_100khz;
+	assign Stepper_Y_M2 = noise_10khz;
+	assign Stepper_Y_En = noise_100khz;
+	assign Stepper_Z_Step = noise_100khz;
+	assign Stepper_Z_Dir = noise_100khz;
+	assign Stepper_Z_M0 = ~noise_100khz;
+	assign Stepper_Z_M1 = noise_100khz;
+	assign Stepper_Z_M2 = noise_100khz;
+	assign Stepper_Z_En = noise_100khz;
+	assign Stepper_A_Step = noise_100khz;
+	assign Stepper_A_Dir = noise_10khz;
+	assign Stepper_A_M0 = noise_10khz;
+	assign Stepper_A_M1 = noise_100khz;
+	assign Stepper_A_M2 = noise_100khz;
+	assign Stepper_A_En = noise_10khz;
+	
+	assign status_led[0] = (& limit)& pause  & encoder_ra & encoder_rb& encoder_ri& encoder_la& encoder_lb & encoder_li&rc_ch1&rc_ch2&rc_ch3&rc_ch4&rc_ch7&rc_ch8&Stepper_X_nFault&Stepper_Y_nFault&Stepper_Z_nFault&Stepper_A_nFault;
 	
 	assign expansion1 = 0;
 	assign expansion2 = 0;
@@ -105,17 +137,19 @@ module UniboardTop(
 	assign motor_pwm_l = 0;
 	assign motor_pwm_r = 0;
 	
-	logic led_intermediate[7:0];
+	
 	logic drdy;
-
-	UARTReceiver #(2083) uart_input(.rx(uart_rx),
-	                                .clk(clk_20MHz),
-				                    .data(led_intermediate),
+	assign debug[8] = drdy;
+	assign status_led[1] = ~drdy;
+	
+	UARTReceiver #(12) uart_input(.rx(uart_rx),
+	                                .clk(clk_12MHz),
+				                    .data(debug[7:0]),
 									.drdy(drdy),
 				                    .reset(0));
-	UARTTransmitter #(2083) uart_output(.tx(uart_tx),
-	                                    .clk(clk_20MHz),
-	                                    .data(led_intermediate),
+	UARTTransmitter #(12) uart_output(.tx(uart_tx),
+	                                    .clk(clk_12MHz),
+	                                    .data(debug[7:0]),
 	                                    .send(drdy),
 	                                    .reset(0));
 endmodule
